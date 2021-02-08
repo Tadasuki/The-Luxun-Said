84188a0d82f20f4ba8d1ef33f9bcaf1b893ea429
Tadasu <39831928+Tadasuki@users.noreply.github.com>
2021-02-09 00:01:43 +0800

    updata

--- run.py
+++ run.py
@@ -101,11 +101,327 @@ infile.close()
 outfile.close()
 
 infile = open("cookie\mid14.txt", "r",encoding='utf-8')
-outfile = open("result.txt", "w",encoding='utf-8')
+outfile = open("cookie\mid15.txt", "w",encoding='utf-8')
 for line in infile:
       outfile.write(line.replace('寒冷', '凄冷'))
 infile.close()
 outfile.close()
+
+infile = open("cookie\mid15.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid16.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('直接', '直截'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid16.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid17.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('让', '令'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid17.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid18.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('于是', '便'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid18.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid19.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('快乐', '快活'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid19.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid20.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('愉悦', '快活'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid20.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid21.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('非常', '十分'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid21.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid22.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('知道', '晓得'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid22.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid23.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('但是', '然而'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid23.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid24.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('就算', '便'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid24.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid25.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('哈欠', '欠身'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid25.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid26.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('灵魂', '魂灵'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid26.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid27.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('分辨', '辨'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid27.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid28.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('变成', '化为'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid28.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid29.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('欢乐', '欢喜'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid29.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid30.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('大致', '大约'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid30.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid31.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('依然', '仍然'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid31.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid32.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('依旧', '仍然'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid32.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid33.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('安静', '寂静'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid33.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid34.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('旗', '纛'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid34.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid35.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('突然', '忽然'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid35.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid36.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('好像', '似乎'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid36.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid37.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('燃烧', '煔烧'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid37.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid38.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('闪烁', '煔'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid38.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid39.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('踪迹', '绳迹'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid39.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid40.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('足迹', '绳迹'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid40.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid41.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('解刨', '釽验'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid41.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid42.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('解剖', '釽验'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid42.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid43.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('阻隔', '沮核'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid43.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid44.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('黄昏', '昏黄'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid44.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid45.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('灵感', '圣觉'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid45.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid46.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('比试', '校雠'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid46.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid47.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('逼迫', '迫拶'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid47.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid48.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('压迫', '迫拶'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid48.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid49.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('道理', '閟机'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid49.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid50.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('玩偶', '人形'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid50.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid51.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('傀儡', '人形'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid51.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid52.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('预', '豫'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid52.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid53.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('恶', '孽'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid53.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid54.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('妖', '孽'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid54.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid55.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('彻底', '澈底'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid55.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid56.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('完全', '澈底'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid56.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid57.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('镀金', '炸大'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid57.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid58.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('恰好', '恰巧'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid58.txt", "r",encoding='utf-8')
+outfile = open("cookie\mid59.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('蝴蝶', '胡蝶'))
+infile.close()
+outfile.close()
+
+infile = open("cookie\mid59.txt", "r",encoding='utf-8')
+outfile = open("result.txt", "w",encoding='utf-8')
+for line in infile:
+      outfile.write(line.replace('不用', '不必'))
+infile.close()
+outfile.close()
+
 # 修改结束
 
 # 删除缓存
@@ -123,5 +439,50 @@ os.remove("cookie\mid11.txt")
 os.remove("cookie\mid12.txt")
 os.remove("cookie\mid13.txt")
 os.remove("cookie\mid14.txt")
+os.remove("cookie\mid15.txt")
+os.remove("cookie\mid16.txt")
+os.remove("cookie\mid17.txt")
+os.remove("cookie\mid18.txt")
+os.remove("cookie\mid19.txt")
+os.remove("cookie\mid20.txt")
+os.remove("cookie\mid21.txt")
+os.remove("cookie\mid22.txt")
+os.remove("cookie\mid23.txt")
+os.remove("cookie\mid24.txt")
+os.remove("cookie\mid25.txt")
+os.remove("cookie\mid26.txt")
+os.remove("cookie\mid27.txt")
+os.remove("cookie\mid28.txt")
+os.remove("cookie\mid29.txt")
+os.remove("cookie\mid30.txt")
+os.remove("cookie\mid31.txt")
+os.remove("cookie\mid32.txt")
+os.remove("cookie\mid33.txt")
+os.remove("cookie\mid34.txt")
+os.remove("cookie\mid35.txt")
+os.remove("cookie\mid36.txt")
+os.remove("cookie\mid37.txt")
+os.remove("cookie\mid38.txt")
+os.remove("cookie\mid39.txt")
+os.remove("cookie\mid40.txt")
+os.remove("cookie\mid41.txt")
+os.remove("cookie\mid42.txt")
+os.remove("cookie\mid43.txt")
+os.remove("cookie\mid44.txt")
+os.remove("cookie\mid45.txt")
+os.remove("cookie\mid46.txt")
+os.remove("cookie\mid47.txt")
+os.remove("cookie\mid48.txt")
+os.remove("cookie\mid49.txt")
+os.remove("cookie\mid50.txt")
+os.remove("cookie\mid51.txt")
+os.remove("cookie\mid52.txt")
+os.remove("cookie\mid53.txt")
+os.remove("cookie\mid54.txt")
+os.remove("cookie\mid55.txt")
+os.remove("cookie\mid56.txt")
+os.remove("cookie\mid57.txt")
+os.remove("cookie\mid58.txt")
+os.remove("cookie\mid59.txt")
 os.rmdir("cookie")
 
